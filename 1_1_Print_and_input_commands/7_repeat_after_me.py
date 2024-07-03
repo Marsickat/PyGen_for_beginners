09a1122ceba477042fd027d8cd04396b4b260b3c
Marsickat <ruslan.marsikcat@yandex.ru>
2024-07-03 16:33:19 +0300

    Решение задач

---
+++ 1_1_Print_and_input_commands/7_repeat_after_me.py
@@ -0,0 +1,27 @@
+# Программа, которая принимает три строки, а затем выводит их в той же последовательности, каждую на отдельной строке
+
+# Пример ввода 1:
+# I was
+# born
+# this way
+# Пример вывода 1:
+# I was
+# born
+# this way
+
+# Пример ввода 2:
+# I love
+# Python
+# so much
+# Пример вывода 2:
+# I love
+# Python
+# so much
+
+# Решение
+first_str = input()
+second_str = input()
+third_str = input()
+print(first_str)
+print(second_str)
+print(third_str)
---
+++ 1_1_Print_and_input_commands/8_repeat_after_me_2.py
@@ -0,0 +1,27 @@
+# Программа, которая принимает три строки, а затем выводит их в обратной последовательности, каждую на отдельной строке
+
+# Пример ввода 1:
+# Hello
+# it's
+# me
+# Пример вывода 1:
+# me
+# it's
+# Hello
+
+# Пример ввода 2:
+# I love
+# Python
+# so much
+# Пример вывода 2:
+# so much
+# Python
+# I love
+
+# Решение
+first_str = input()
+second_str = input()
+third_str = input()
+print(third_str)
+print(second_str)
+print(first_str)
